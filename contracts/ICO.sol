2cca56165023032aedc3ab6f53ea40560ecaaf36
Sergi Juanati <wint3rmute@gmail.com>
2021-03-08 16:54:50 +0100

    ICO contract with struct Investor

--- contracts/ICO.sol
+++ contracts/ICO.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 
-import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol';
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract newERC20 is ERC20 {
-    
-    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {}
-    
+    constructor(string memory name_, string memory symbol_)
+        ERC20(name_, symbol_)
+    {}
+
     function mint(address account, uint256 amount) public {
         _mint(account, amount);
     }
@@ -14,20 +15,28 @@ contract newERC20 is ERC20 {
 
 contract ICO {
     enum Status {ACTIVE, FROZEN, CLOSED}
-    
+
+    struct Investor {
+        uint256 amount;
+        bool isWithdrawn;
+    }
+
     struct Ico {
         address founder;
         uint256 totalTokens;
         Status status;
         newERC20 token;
-        mapping(address => uint256) investors;
-        mapping(address => bool) isAllocated;
+        mapping(address => Investor) investors;
     }
-    
+
     uint256 numIco;
     mapping(uint256 => Ico) public icos;
-    
-    function startICO(string memory name, string memory symbol, uint256 totalTokens) external {
+
+    function startICO(
+        string memory name,
+        string memory symbol,
+        uint256 totalTokens
+    ) external {
         Ico storage ico = icos[numIco];
         ico.founder = msg.sender;
         ico.totalTokens = totalTokens;
@@ -37,33 +46,44 @@ contract ICO {
         numIco += 1;
         //todo: create an ERC20 token and store its reference
     }
-    
+
     function contribute(uint256 _numIco) external payable {
         Ico storage ico = icos[_numIco];
-        require(ico.status == Status.ACTIVE, 'ICO is not active');
-        ico.investors[msg.sender] += msg.value;
+        require(ico.status == Status.ACTIVE, "ICO is not active");
+        //ico.investors[msg.sender] += msg.value;
+        ico.investors[msg.sender].amount += msg.value;
     }
-    
+
     function closeICO(uint256 _numIco) external {
         Ico storage ico = icos[_numIco];
-        require(ico.founder == msg.sender, 'only founder can close ICO');
+        require(ico.founder == msg.sender, "only founder can close ICO");
         ico.status = Status.CLOSED;
     }
-    
+
     function claimTokens(uint256 _numIco) external {
-        //todo: require only when ICO is closed
         Ico storage ico = icos[_numIco];
-        require(ico.isAllocated[msg.sender] == false, 'tokens already claimed');
-        require(ico.investors[msg.sender] > 0, 'no amount to be claimed');
-        ico.token.transfer(msg.sender, ico.investors[msg.sender] * 2);
-        ico.isAllocated[msg.sender] = true;
+        require(ico.status == Status.CLOSED, "ico not yet close");
+        require(
+            ico.investors[msg.sender].isWithdrawn == false,
+            "tokens already claimed"
+        );
+        require(
+            ico.investors[msg.sender].amount > 0,
+            "no amount to be claimed"
+        );
+        ico.token.transfer(msg.sender, ico.investors[msg.sender].amount * 2);
+        ico.investors[msg.sender].isWithdrawn = true;
     }
-    
-    function checkContribution(uint256 _numIco) external view returns(uint256){
-        return icos[_numIco].investors[msg.sender];
+
+    function checkContribution(uint256 _numIco)
+        external
+        view
+        returns (uint256)
+    {
+        return icos[_numIco].investors[msg.sender].amount;
     }
-    
-    function checkTokens(uint256 _numIco) external view returns(uint256){
+
+    function checkTokens(uint256 _numIco) external view returns (uint256) {
         return icos[_numIco].token.balanceOf(msg.sender);
     }
 }
